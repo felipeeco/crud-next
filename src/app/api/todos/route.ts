c4c2cf6facf13cf362c18a98fdd8ee99a6c6248b
Luis Felipe Moreno Rondon <luisfelipe.moreno.contractor@bbva.com>
2024-10-21 16:37:13 -0500

    feat(*): create id service

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,9 +1,24 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: Request) {
 
-  const todos = await prisma.todo.findMany();
+  const { searchParams } = new URL(request.url);
+  const take = searchParams.get("take") ?? "10";
+  const skip = searchParams.get("skip") ?? "0";
+
+  if(isNaN(+skip)) {
+    return NextResponse.json({ message: "skip parameter should be a number" }, { status: 400 });
+  }
+
+  if(isNaN(+take)) {
+    return NextResponse.json({ message: "take parameter should be a number" }, { status: 400 });
+  }
+
+  const todos = await prisma.todo.findMany({
+    take: +take,
+    skip: +skip
+  });
 
   return NextResponse.json(todos);
 }
