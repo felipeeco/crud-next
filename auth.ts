395f2e0238d549b1aa22df7794ff604557a053e8
Luis Felipe Moreno Rondon <luisfelipe.moreno.contractor@bbva.com>
2024-12-05 23:05:08 -0500

    feat(*): create google authentication

--- auth.ts
+++ auth.ts
@@ -1,8 +1,13 @@
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions:NextAuthOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    }),
     GithubProvider({
       clientId: process.env.AUTH_GITHUB_ID ?? '',
       clientSecret: process.env.AUTH_GITHUB_SECRET ?? ''
