e32a5ee7526f179d42c35153b4fea41c036543f7
Luis Felipe Moreno Rondon <luisfelipe.moreno.contractor@bbva.com>
2024-12-07 17:57:23 -0500

    feat(*): create roles user

--- auth.ts
+++ auth.ts
@@ -15,5 +15,26 @@ export const authOptions:NextAuthOptions = {
       clientId: process.env.AUTH_GITHUB_ID ?? '',
       clientSecret: process.env.AUTH_GITHUB_SECRET ?? ''
     })
-  ]
+  ],
+  session: {
+    strategy: 'jwt'
+  },
+  callbacks: {
+    async signIn(/* {user, account, profile, email, credentials} */) {
+      return true;
+    },
+    async jwt({token, /* user, account, profile */}) {
+      const dbUser = await prisma.user.findUnique({ where: { email: token.email ?? 'no-email' } });
+      token.roles = dbUser?.roles ?? ['no-roles'];
+      token.id = dbUser?.id ?? 'no-uuid';
+      return token
+    },
+    async session({session, token, /*  user */}) {
+      if ( session && session.user ) {
+        session.user.roles = token.roles;
+        session.user.id = token.id;
+      }
+      return session
+    }
+  }
 }
